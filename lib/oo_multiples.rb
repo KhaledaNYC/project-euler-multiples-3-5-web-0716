d1f944946dd9d7d5da7c85d2b522b381697463c8
KhaledaNYC <khaleda.khan87@gmail.com>
2016-10-16 13:52:06 -0400

    multiples of 3 and five

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,23 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :limit
+  def initialize(limit)
+    @limit = limit
+  end
+  def collect_multiples
+    i = 1
+    multiples = []
+    while i < limit
+      if (i % 3 == 0 || i % 5 == 0)
+        multiples << i
+     end
+       i += 1
+   end
+    return multiples
+  end
+
+  def sum_multiples
+    multiples = collect_multiples()
+    multiples.reduce(:+)
+  end
+end
